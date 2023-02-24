d3c51d4d5128907ee455490d5544db40c68637dc
Todd Albert <todd@bocacode.com>
2023-02-24 15:01:43 -0500

    regular fetch of secret data

--- src/scenes/Content.jsx
+++ src/scenes/Content.jsx
@@ -1,3 +1,20 @@
+import { useEffect, useState } from "react"
+
 export default function Content() {
-  return <h1>Content</h1>
+  const [secrets, setSecrets] = useState()
+  useEffect(() => {
+    fetch('http://localhost:5002/secrets')
+      .then(res => res.json())
+      .then(setSecrets)
+      .catch(alert)
+  }, [])
+  return (
+    <>
+      <h1>Content</h1>
+      {secrets
+        ? <h2>{secrets.message}</h2>
+        : <h2>Loading...</h2>
+      }
+    </>
+  )
 }
