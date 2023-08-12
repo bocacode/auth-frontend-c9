92054c99cb04dc53e815dc9d99cec110df9f08f8
Daniel Barba <frdb103@gmail.com>
2023-08-12 19:04:46 -0400

    update

--- src/scenes/Content.jsx
+++ src/scenes/Content.jsx
@@ -1,3 +1,27 @@
+import { useEffect, useState } from "react"
+
 export default function Content() {
-  return <h1>Content</h1>
+  const [secrets,setSecrets] = useState()
+
+  useEffect(()=>{
+    fetch("http://127.0.0.1:5002/secrets", {
+      headers:{
+          Authorization: localStorage.getItem("token")
+      }
+    })
+    .then(incoming=>incoming.json())
+    .then(setSecrets)
+    .catch(console.error)
+  },[])
+
+  return(
+    <>
+    <h1>Content</h1>
+    {secrets
+    ?<h2>{secrets.message}</h2>
+    :<h2>Loading...</h2>
+    }
+    </>
+    )
+    
 }
